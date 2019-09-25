bdf413efeb6fcc26cc4b97ce082b66313ecdcdb6
MacroUniverse <270174408@qq.com>
2019-09-24 21:41:56 -0500

    official tutorial (step 2)

--- tutorial.cxx
+++ tutorial.cxx
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "TutorialConfig.h"
 
 int main()
 {
 	using namespace std;
 	cout << "hello!" << endl;
+	cout << "version: " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << endl;
 }
