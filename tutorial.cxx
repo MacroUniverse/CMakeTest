56fefd4d7db6127e698a1bbc9a8fa5f6bacece5c
MacroUniverse <270174408@qq.com>
2019-09-25 02:43:27 -0500

    step 4

--- tutorial.cxx
+++ tutorial.cxx
@@ -1,11 +1,17 @@
 #include <iostream>
 #include "TutorialConfig.h"
+#ifdef USE_MYMATH
 #include "MathFunctions/sqr.h"
+#endif
 
 int main()
 {
 	using namespace std;
 	cout << "hello!" << endl;
 	cout << "version: " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << endl;
+#ifdef USE_MYMATH
 	cout << "1.5^2 = " << sqr(1.5) << endl;
+#else
+	cout << "1.5^2 = " << 1.5*1.5 << endl;
+#endif
 }
